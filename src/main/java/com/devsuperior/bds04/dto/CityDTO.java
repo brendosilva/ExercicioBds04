5b02d74b852f910cb5ec9d225b3b0345945fdf1d
Brendo Silva <breendo.01@gmail.com>
2022-05-31 10:24:53 -0300

    resolução dasafio

--- src/main/java/com/devsuperior/bds04/dto/CityDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/CityDTO.java
@@ -1,15 +1,20 @@
 package com.devsuperior.bds04.dto;
 
+
 import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
 
 import com.devsuperior.bds04.entities.City;
 
 public class CityDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
+
+	@NotBlank(message ="Campo requerido")
 	private String name;
-	
+
 	public CityDTO() {
 	}
 
@@ -17,7 +22,7 @@ public class CityDTO implements Serializable {
 		this.id = id;
 		this.name = name;
 	}
-	
+
 	public CityDTO(City entity) {
 		id = entity.getId();
 		name = entity.getName();
--- src/main/java/com/devsuperior/bds04/entities/City.java
+++ src/main/java/com/devsuperior/bds04/entities/City.java
@@ -1,5 +1,6 @@
 package com.devsuperior.bds04.entities;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,15 +14,15 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_city")
 public class City {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@OneToMany(mappedBy = "city")
 	private List<Event> events = new ArrayList<>();
-	
+
 	public City() {
 	}
 
